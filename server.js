d8c686393db973a50b480f2f88ac5792aff09d44
Harsh Bhardwaj <harshbhardwaj860@gmail.com>
2024-06-28 00:41:11 +0530

    code cleanup

--- server.js
+++ server.js
@@ -1,76 +1,3 @@
-// import express from 'express';
-// import http from 'http';
-// import { Server } from 'socket.io';
-// import cors from 'cors'
-// import Actions from "./src/action.js";
-// const app = express();
-// const server = http.createServer(app);
-
-// app.use(cors());
-// const io = new Server(server, {
-//   cors: {
-//     origin: "http://localhost:5173",
-//     methods: ["GET", "POST"],
-//   },
-// });
-// const socketMapper = {};
-// function getAllCollectedClientInRoom(roomId) {
-//   const clients = [];
-//   const clientsInRoom = io.sockets.adapter.rooms.get(roomId) || new Set();
-//   clientsInRoom.forEach((socketId) => {
-//     const clientSocket = io.sockets.sockets.get(socketId);
-//     if (clientSocket) {
-//       clients.push({
-//         socketId: clientSocket.id,
-//         userName: socketMapper[socketId],
-//       });
-//     }
-//   });
-//   return clients;
-// }
-
-// io.on("connection", (socket) => {
-//   console.log("a user connected", socket.id);
-//   socket.on(Actions.JOIN, ({ roomId, userName }) => {
-//     socketMapper[socket.id] = userName;
-//     socket.join(roomId);
-//     const client = getAllCollectedClientInRoom(roomId);
-//     console.log(client)
-//     io.to(roomId).emit(Actions.JOINED, {
-//       client,
-//       userName,
-//       socketId: socket.id,
-//     });
-//   });
-
-//   socket.on(Actions.CODE_CHANGE, ({roomId,newCode}) => {
-//     // Broadcast the code change to all other clients
-//     io.to(roomId).emit(Actions.CODE_CHANGE, {
-//       newCode
-//     });
-//     console.log(`Code change broadcasted to room ${roomId}`);
-//   });
-
-//   socket.on("disconnect", (roomId) => {
-//     console.log("user disconnected");
-//   });
-
-//   socket.on(Actions.LEAVE,({roomId})=>{
-//     console.log("huuufuug")
-//     const client = getAllCollectedClientInRoom(roomId);
-//     console.log(client);
-//     io.to(roomId).emit(Actions.LEAVE, {
-//       client
-//     });
-//   })
-
-// });
-
-// server.listen(4000, () => {
-//   console.log("listening on 4000");
-// });
-
-
 import express from 'express';
 import http from 'http';
 import { Server } from 'socket.io';
@@ -130,10 +57,6 @@ io.on("connection", (socket) => {
   socket.on(Actions.LEAVE, ({ roomId }) => {
     socket.leave(roomId);
     const client = getAllCollectedClientInRoom(roomId);
-    // io.to(roomId).emit(Actions.LEAVE, {
-    //   client,
-    //   userName: socketMapper[socket.id]
-    // });
     console.log("client",client);
     socket.broadcast.to(roomId).emit(Actions.LEAVE, {
         client,
@@ -146,11 +69,7 @@ io.on("connection", (socket) => {
     // Remove the user from the socketMapper
     const rooms = Array.from(socket.rooms);
     rooms.forEach((roomId) => {
-     // socket.leave(roomId);
-      // const client = getAllCollectedClientInRoom(roomId);
-      // io.to(roomId).emit(Actions.LEAVE, {
-      //   client,
-      // });
+
     });
     delete socketMapper[socket.id];
   });
